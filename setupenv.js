97ec4705fabb01db7815b792cf23ce746399fa87
Not Thomiz <jeanouina@gmail.com>
2022-03-07 21:47:54 +0100

    fucked up networks

--- setupenv.js
+++ setupenv.js
@@ -1,7 +1,7 @@
 require("fs").writeFileSync("./.env", `REACT_APP_NAME=$npm_package_name
 REACT_APP_VERSION=$npm_package_version
 
-REACT_APP_NETWORKS=1,2,4
-REACT_APP_DEFAULT_NETWORK=2
+REACT_APP_NETWORKS=1
+REACT_APP_DEFAULT_NETWORK=1
 PUBLIC_URL=
 REACT_APP_IS_DEV=false`)
