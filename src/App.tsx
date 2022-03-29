74fdf52d1bd4c92bcaa7bdef9e78a207f286c667
Not Thomiz <jeanouina@gmail.com>
2022-03-29 22:15:51 +0200

    Fix Linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import { ApolloProvider } from '@apollo/client';
-import { styled } from '@mui/material';
 import BigNumber from 'bignumber.js';
 import { SnackbarProvider } from 'notistack';
 import React from 'react';
-import BackgroundSVG from './bg.svg';
 import { getApolloClient } from './clients/apollo.client';
 import { ConnectedWeb3 } from './contexts/connectedWeb3';
 import { Web3Provider } from './contexts/web3';
 import { Main } from './features/main/components/main';
-import Box from '@mui/material/Box';
-
-const Background = styled('div')(`
-  width: 100%;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-  background-image: url('${BackgroundSVG}');
-`
-);
 
 const App: React.FC = () => {
   const apolloClient = React.useMemo(() => getApolloClient(), [])
