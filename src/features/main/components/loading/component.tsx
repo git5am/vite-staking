6f7f46b2199133827b0d145378989b699c9a4beb
ispan <ispanev@gmail.com>
2022-04-01 21:32:24 +0200

    change Loading...

--- src/features/main/components/loading/component.tsx
+++ src/features/main/components/loading/component.tsx
@@ -1,19 +1,24 @@
 
-import { Typography } from '@mui/material';
-import { Footer } from '../../../layout/components/footer';
+import { Typography, Grid, CircularProgress } from '@mui/material';
+//import { Footer } from '../../../layout/components/footer';
 import { HeaderLoading } from '../../../layout/components/header-loading';
 import { MainScroll } from '../../../layout/components/main-scroll';
 import { MainWrapper } from '../../../layout/components/main-wrapper';
 
 export const MainLoading = () => {
-
   return (
     <MainWrapper>
       <HeaderLoading />
       <MainScroll>
-        <Typography>Loading...</Typography>
+        <Grid container justifyContent="center" alignItems="center" flex={1}>
+        <Typography sx={{mb: 2}}>
+          Loading... 
+        </Typography>
+        </Grid>
+        <Grid container justifyContent="center" alignItems="center" flex={1}>
+          <CircularProgress />
+        </Grid>
       </MainScroll>
-      <Footer />
     </MainWrapper>
   )
 }
